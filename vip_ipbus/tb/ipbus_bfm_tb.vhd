606cc1a69b925855a015d8cfad84abbfc6d2ea7c
Michał Kruszewski <mkru@protonmail.com>
2019-05-25 06:24:22 +0200

    Add license headers to vip_ipbus files.

--- vip_ipbus/tb/ipbus_bfm_tb.vhd
+++ vip_ipbus/tb/ipbus_bfm_tb.vhd
@@ -1,3 +1,19 @@
+--========================================================================================================================
+-- Copyright (c) 2017 by Bitvis AS.  All rights reserved.
+
+-- You should have received a copy of the license file containing the MIT License (see LICENSE.TXT), if not,
+-- contact Bitvis AS <support@bitvis.no>.
+--========================================================================================================================
+-- Copyright (c) 2019 by Michał Kruszewski. All rights reserved.
+--
+-- All IPbus Bus Functional Model files are provided with the same MIT License as the rest of the UVVM infrastrucutre.
+--=======================================================================================================================
+-- UVVM AND ANY PART THEREOF ARE PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+-- WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS
+-- OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+-- OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH UVVM OR THE USE OR OTHER DEALINGS IN UVVM.
+--========================================================================================================================
+
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
